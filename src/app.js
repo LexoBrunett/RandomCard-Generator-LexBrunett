5270b3ccea4cd7ee070b82f99ff5a78b7b9aae9d
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-12 02:20:42 +0000

    modifing some icons

--- src/app.js
+++ src/app.js
@@ -5,26 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// creacion body
-const body = document.body;
-body.style.background = "green";
+icons = ["♥", "♠", "♣", "♦"];
+card_numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
-const card = document.createElement("DIV");
-card.classList.add("card");
-body.append(card);
+window.onload = function() {
+  //write your code here
+  // creacion body
+  const body = document.body;
+  body.style.background = "green";
 
-//creacion div sup-izq
-const SupIzq = document.createElement("DIV");
-const centro = document.createElement("DIV");
-const inferiorDerecha = document.createElement("DIV");
+  const card = document.createElement("DIV");
+  card.classList.add("card");
+  body.append(card);
 
-SupIzq.classList.add("SupIzq");
-centro.classList.add("centro");
-inferiorDerecha.classList.add("inferiorDerecha");
+  //creacion div sup-izq
+  const UpLeft = document.createElement("DIV");
+  const Center = document.createElement("DIV");
+  const DownRight = document.createElement("DIV");
 
-SupIzq.innerHTML = "♣";
-centro.innerHTML = "8";
-inferiorDerecha.innerHTML = "♦";
-card.append(SupIzq);
-card.append(centro);
-card.append(inferiorDerecha);
+  UpLeft.classList.add("UpLeft");
+  Center.classList.add("Center");
+  DownRight.classList.add("DownRight");
+
+  UpLeft.innerHTML = icons[getRandom(icons.length)];
+  Center.innerHTML = card_numbers[getRandom(card_numbers.length)];
+  DownRight.innerHTML = icons[getRandom(icons.length)];
+  card.append(UpLeft);
+  card.append(Center);
+  card.append(DownRight);
+};
+
+function getRandom(arrayLength) {
+  let number = Math.floor(Math.random() * arrayLength);
+  return number;
+}
