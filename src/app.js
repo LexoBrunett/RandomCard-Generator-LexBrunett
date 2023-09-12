c7b26b9b0cf28a42d060f046c118ef066b32e889
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-12 02:22:40 +0000

    adding let

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-icons = ["♥", "♠", "♣", "♦"];
-card_numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let icons = ["♥", "♠", "♣", "♦"];
+let card_numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
 window.onload = function() {
   //write your code here
