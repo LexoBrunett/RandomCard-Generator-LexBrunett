d8f724d710662bd091b523711764bd76eb155e97
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-12 02:18:38 +0000

    Js modifications

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// creacion body
+const body = document.body;
+body.style.background = "green";
+
+const card = document.createElement("DIV");
+card.classList.add("card");
+body.append(card);
+
+//creacion div sup-izq
+const SupIzq = document.createElement("DIV");
+const centro = document.createElement("DIV");
+const inferiorDerecha = document.createElement("DIV");
+
+SupIzq.classList.add("SupIzq");
+centro.classList.add("centro");
+inferiorDerecha.classList.add("inferiorDerecha");
+
+SupIzq.innerHTML = "♣";
+centro.innerHTML = "8";
+inferiorDerecha.innerHTML = "♦";
+card.append(SupIzq);
+card.append(centro);
+card.append(inferiorDerecha);
